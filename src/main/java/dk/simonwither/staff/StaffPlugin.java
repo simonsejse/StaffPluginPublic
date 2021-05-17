30bd8d17b2d4a92336683fae7f500cdc22b8c0b2
simonsejse <simonwa01@gmail.com>
2021-05-17 15:23:34 +0200

    Initial commit

---
+++ src/main/java/dk/simonwither/staff/StaffPlugin.java
@@ -0,0 +1,19 @@
+package dk.simonwither.staff;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class StaffPlugin extends JavaPlugin {
+
+    
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
